c940586770322af72a0edc81cbf5a5af86ca8db8
Cedric1425 <118215545+Cedric1425@users.noreply.github.com>
2023-03-16 17:32:58 +0100

    Création de la classe sommet
    Contient uniquement un entier qui représente la clé

---
+++ sommet.cpp
@@ -0,0 +1,12 @@
+#include "sommet.h"
+
+sommet::sommet( int cle) : d_cle {cle}
+{}
+
+
+int sommet::getCle()
+{
+    return d_cle;
+}
+
+
---
+++ sommet.h
@@ -0,0 +1,14 @@
+#ifndef SOMMET_H
+#define SOMMET_H
+
+
+class sommet {
+public :
+    sommet (int cle);
+    int getCle();
+
+private :
+    int d_cle;
+};
+
+#endif
