f908894f1f763657ab2ed5784cc4e999dc6cbf47
Cedric1425 <118215545+Cedric1425@users.noreply.github.com>
2023-03-16 17:50:32 +0100

    Extracteur creation
    Sert à extraire un graph à partir d'un fichier
    Il faudra donc créer nos 3 types de fichiers à faire passer dedans

---
+++ extracteur.cpp
@@ -0,0 +1,1 @@
+#include "extracteur.h"
---
+++ extracteur.h
@@ -0,0 +1,16 @@
+#ifndef EXTRACTEUR_H
+#define EXTRACTEUR_H
+
+#include "graph.h"
+#include <string>
+
+class extracteur {
+    extracteur();
+
+    graph extractFrom( std::string nomFichier);
+
+
+};
+
+
+#endif // EXTRACTEUR_H
