20777eaf929f27c34e01d1ea92befdd4f8364c3f
Samib68 <118477587+Samib68@users.noreply.github.com>
2023-04-26 18:52:28 +0200

    ajout lecture de graphe

--- graphenonoriente.h
+++ graphenonoriente.h
@@ -2,6 +2,7 @@
 #define GRAPHENONORIENTE_H
 
 #include "graphe.h"
+#include <string>
 
 class GrapheNonOriente : public Graphe
 {
@@ -10,7 +11,7 @@ public:
     GrapheNonOriente(const vector<vector<Sommet>>& matrice, int n, int m);
     GrapheNonOriente(const vector<Sommet>& fs, const vector<int>& aps, int n, int m);
     GrapheNonOriente(const vector<Chainon>& lp, const vector<Chainon>& ls, int n, int m);
-
+    bool lireGrapheNonOriente(std::string nomFic);
     void rang(vector<int>& rang) override;
     bool codagePrufer (vector <int> & prufer) override;
     bool calculDistance (int racine, vector<int> &dist) override;
